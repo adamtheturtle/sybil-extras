3bf56aec012d5d935c2f84384973454642c32b60
Adam Dangoor <adamdangoor@gmail.com>
2024-09-07 11:04:18 +0100

    Passing tsst for success case

--- tests/test_shell_evaluator.py
+++ tests/test_shell_evaluator.py
@@ -24,8 +24,8 @@ def rst_file_fixture(tmp_path: Path) -> Path:
     return test_document
 
 
-def test_shell_command_evaluator_runs(rst_file: Path) -> None:
-    """Test that ShellCommandEvaluator successfully runs a shell command."""
+def test_output_shown_on_error(rst_file: Path) -> None:
+    """Stdout and Stderr are shown when a command fails."""
     evaluator = ShellCommandEvaluator(
         args=[
             "bash",
@@ -55,38 +55,51 @@ def test_shell_command_evaluator_runs(rst_file: Path) -> None:
         example.evaluate()
 
 
-def test_shell_command_evaluator_with_failure(rst_file: Path) -> None:
-    """Test that ShellCommandEvaluator handles a failing command."""
+def test_no_output_on_success(
+    rst_file: Path,
+    tmp_path: Path,
+    capsys: pytest.CaptureFixture[str],
+) -> None:
+    """No output is shown when a command succeeds."""
+    new_file = tmp_path / "new_file.txt"
     evaluator = ShellCommandEvaluator(
-        args=["bash", "-c", "exit 1"], pad_file=False, write_to_file=False
-    )
-    parser = CodeBlockParser(language="shell", evaluator=evaluator)
-    sybil = Sybil(parsers=[parser])
-
-    document = sybil.parse(path=rst_file)
-    (example,) = list(document)
-
-    # Expecting an error to be raised due to the command failure
-    with pytest.raises(subprocess.CalledProcessError):
-        example.evaluate()
-
-
-def test_shell_command_evaluator_with_environment(rst_file: Path) -> None:
-    """Test that ShellCommandEvaluator respects environment variables."""
-    evaluator = ShellCommandEvaluator(
-        args=["bash", "-c", "echo $TEST_ENV"],
-        env={"TEST_ENV": "SybilTestEnv"},
+        args=[
+            "bash",
+            "-c",
+            f"echo 'Hello, Sybil!' > {new_file}",
+        ],
         pad_file=False,
         write_to_file=False,
     )
-    parser = CodeBlockParser(language="shell", evaluator=evaluator)
+    parser = CodeBlockParser(language="python", evaluator=evaluator)
     sybil = Sybil(parsers=[parser])
 
     document = sybil.parse(path=rst_file)
     (example,) = list(document)
-
-    # Evaluate the shell command
     example.evaluate()
+    new_file_content = new_file.read_text(encoding="utf-8")
+    assert new_file_content == "Hello, Sybil!\n"
+    outerr = capsys.readouterr()
+    assert outerr.out == ""
+    assert outerr.err == ""
 
-    # Check if the environment variable was echoed in the output
-    assert example.namespace["output"] == "SybilTestEnv\n"
+
+# def test_shell_command_evaluator_with_environment(rst_file: Path) -> None:
+#     """Test that ShellCommandEvaluator respects environment variables."""
+#     evaluator = ShellCommandEvaluator(
+#         args=["bash", "-c", "echo $TEST_ENV"],
+#         env={"TEST_ENV": "SybilTestEnv"},
+#         pad_file=False,
+#         write_to_file=False,
+#     )
+#     parser = CodeBlockParser(language="shell", evaluator=evaluator)
+#     sybil = Sybil(parsers=[parser])
+
+#     document = sybil.parse(path=rst_file)
+#     (example,) = list(document)
+
+#     # Evaluate the shell command
+#     example.evaluate()
+
+#     # Check if the environment variable was echoed in the output
+#     assert example.namespace["output"] == "SybilTestEnv\n"
