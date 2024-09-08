464870d71446a00e0668adc802dfcda653b911d4
Adam Dangoor <adamdangoor@gmail.com>
2024-09-08 23:41:24 +0100

    Remove unnecessary conftest run

--- conftest.py
+++ conftest.py
@@ -1,22 +1,15 @@
 """Setup for Sybil."""
 
-import os
-import sys
 from doctest import ELLIPSIS
 
 import pytest
 from beartype import beartype
 from sybil import Sybil
-from sybil.evaluators.python import PythonEvaluator
 from sybil.parsers.rest import (
     ClearNamespaceParser,
-    CodeBlockParser,
     DocTestParser,
+    PythonCodeBlockParser,
 )
-from sybil.sybil import SybilCollection
-
-from sybil_extras.evaluators.multi import MultiEvaluator
-from sybil_extras.evaluators.shell_evaluator import ShellCommandEvaluator
 
 
 @beartype
@@ -29,67 +22,11 @@ def pytest_collection_modifyitems(items: list[pytest.Item]) -> None:
             item.obj = beartype(obj=item.obj)
 
 
-sybil = Sybil(
+pytest_collect_file = Sybil(
     parsers=[
         ClearNamespaceParser(),
         DocTestParser(optionflags=ELLIPSIS),
-        CodeBlockParser(
-            language="shell",
-            evaluator=ShellCommandEvaluator(
-                args=[
-                    sys.executable,
-                    "-m",
-                    "pre_commit",
-                    "run",
-                    "--files",
-                ],
-                tempfile_suffix=".sh",
-                pad_file=False,
-                write_to_file=False,
-            ),
-        ),
-        CodeBlockParser(
-            language="python",
-            evaluator=MultiEvaluator(
-                evaluators=[
-                    PythonEvaluator(),
-                    ShellCommandEvaluator(
-                        args=[
-                            sys.executable,
-                            "-m",
-                            "pre_commit",
-                            "run",
-                            "ruff-format-fix",
-                            "--files",
-                        ],
-                        tempfile_suffix=".py",
-                        pad_file=False,
-                        write_to_file=True,
-                    ),
-                    ShellCommandEvaluator(
-                        args=[
-                            sys.executable,
-                            "-m",
-                            "pre_commit",
-                            "run",
-                            "--files",
-                        ],
-                        env={
-                            **os.environ,
-                            # These hooks are run separately as they do not
-                            # work with file padding.
-                            "SKIP": "ruff-format-diff",
-                        },
-                        tempfile_suffix=".py",
-                        pad_file=True,
-                        write_to_file=True,
-                    ),
-                ]
-            ),
-        ),
+        PythonCodeBlockParser(),
     ],
     patterns=["*.rst", "*.py"],
-)
-
-sybils = SybilCollection([sybil])
-pytest_collect_file = sybils.pytest()
+).pytest()
