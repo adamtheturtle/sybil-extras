4635228d5343ea2012af59c07c4e7c01081ce4a7
Adam Dangoor <adamdangoor@gmail.com>
2024-09-08 21:48:37 +0100

    Working CLI

--- format_fix_docs.py
+++ format_fix_docs.py
@@ -1,10 +1,11 @@
 """Script to run ruff format on the given file."""
 
-import sys
+import shlex
 from collections.abc import Iterable, Sequence
 from pathlib import Path
 
 import click
+from beartype import beartype
 from sybil import Sybil
 from sybil.parsers.markdown import CodeBlockParser as MarkdownCodeBlockParser
 from sybil.parsers.rest import CodeBlockParser as RestCodeBlockParser
@@ -12,6 +13,7 @@ from sybil.parsers.rest import CodeBlockParser as RestCodeBlockParser
 from sybil_extras.evaluators.shell_evaluator import ShellCommandEvaluator
 
 
+@beartype
 def _run_args_against_docs(
     file_path: Path,
     args: Sequence[str | Path],
@@ -36,17 +38,18 @@ def _run_args_against_docs(
         example.evaluate()
 
 
+@beartype
 @click.command()
-@click.argument("file_paths", type=click.Path(exists=True), nargs=-1)
-def main(file_paths: Iterable[Path]) -> None:
+@click.option("language", "--language", type=str, required=True)
+@click.option("command", "--command", type=str, required=True)
+@click.argument(
+    "file_paths",
+    type=click.Path(exists=True, path_type=Path),
+    nargs=-1,
+)
+def main(language: str, command: str, file_paths: Iterable[Path]) -> None:
     """Run ruff format on the given files."""
-    args = [
-        sys.executable,
-        "-m",
-        "ruff",
-        "format",
-    ]
-    language = "python"
+    args = shlex.split(s=command)
     for file_path in file_paths:
         _run_args_against_docs(
             args=args,
