3c20e13d1a85b087d77bf12f82be7d9a8ee5ceb8
Adam Dangoor <adamdangoor@gmail.com>
2024-09-22 19:19:27 +0100

    Add tests for MyST

--- src/sybil_extras/parsers/markdown/custom_directive_skip.py
+++ src/sybil_extras/parsers/markdown/custom_directive_skip.py
@@ -1,18 +1,18 @@
-"""A custom directive skip parser for reST."""
+"""A custom directive skip parser for Markdown."""
 
 from sybil.parsers.abstract import AbstractSkipParser
 from sybil.parsers.markdown.lexers import DirectiveInHTMLCommentLexer
 
 
 class CustomDirectiveSkipParser(AbstractSkipParser):
-    """A custom directive skip parser for reST."""
+    """A custom directive skip parser for Markdown."""
 
     def __init__(self, directive: str) -> None:
         """
         Args:
             directive: The name of the directive to skip.
         """
-        # This matches the ``sybil.parsers.rest.SkipParser``, other than
+        # This matches the ``sybil.parsers.markdown.SkipParser``, other than
         # it does not hardcode the directive "skip".
         super().__init__(
             lexers=[DirectiveInHTMLCommentLexer(directive=directive)],
