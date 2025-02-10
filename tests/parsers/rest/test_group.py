244c1c2240a3a474902e8c10d8f6290954967cd0
Adam Dangoor <adamdangoor@gmail.com>
2025-02-10 00:32:32 +0000

    Failing test for skip in a group

--- tests/parsers/rest/test_group.py
+++ tests/parsers/rest/test_group.py
@@ -7,6 +7,7 @@ from textwrap import dedent
 
 from sybil import Sybil
 from sybil.parsers.rest.codeblock import PythonCodeBlockParser
+from sybil.parsers.rest.skip import SkipParser
 
 from sybil_extras.parsers.rest.group import GroupParser
 
@@ -158,6 +159,65 @@ def test_empty_group(tmp_path: Path) -> None:
     assert parsed_examples == expected
 
     assert document.namespace["x"] == [3]
+
+
+def test_group_with_skip(tmp_path: Path) -> None:
+    """
+    The group parser groups examples even when the group is skipped.
+    """
+    content = """\
+
+    .. code-block:: python
+
+        x = []
+
+    .. group: start
+
+    .. code-block:: python
+
+        x = [*x, 1]
+
+    .. skip: next
+
+    .. code-block:: python
+
+        x = [*x, 2]
+
+    .. group: end
+
+    .. code-block:: python
+
+        x = [*x, 3]
+    """
+
+    test_document = tmp_path / "test.rst"
+    test_document.write_text(data=content, encoding="utf-8")
+
+    group_parser = GroupParser(directive="group")
+    code_parser = PythonCodeBlockParser()
+    skip_parser = SkipParser()
+
+    sybil = Sybil(parsers=[code_parser, group_parser, skip_parser])
+    document = sybil.parse(path=test_document)
+
+    for example in document.examples():
+        example.evaluate()
+
+    parsed_examples = [example.parsed for example in document.examples()]
+    expected = [
+        "x = []\n",
+        dedent(
+            text="""\
+            x = [*x, 1]
+
+            x = [*x, 2]
+            """
+        ),
+        "x = [*x, 3]\n",
+    ]
+    assert parsed_examples == expected
+
+    assert document.namespace["x"] == [1, 2]
 
 
 # TODO: With skips before / in the middle
