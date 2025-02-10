384522e4d822be437ab547096123a684f70c8985
Adam Dangoor <adamdangoor@gmail.com>
2025-02-10 00:58:28 +0000

    Improve multiple docstrings as spotted while building the group parser

--- src/sybil_extras/parsers/markdown/custom_directive_skip.py
+++ src/sybil_extras/parsers/markdown/custom_directive_skip.py
@@ -18,7 +18,7 @@ class CustomDirectiveSkipParser:
     def __init__(self, directive: str) -> None:
         """
         Args:
-            directive: The name of the directive to skip.
+            directive: The name of the directive to use for skipping.
         """
         # This matches the ``sybil.parsers.markdown.SkipParser``, other than
         # it does not hardcode the directive "skip".
