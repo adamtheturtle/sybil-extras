c97c26ce1cb0b4efde11341c3023adda4bf6b9c7
Adam Dangoor <adamdangoor@gmail.com>
2025-02-10 00:06:38 +0000

    Test for empty group

--- tests/parsers/rest/test_group.py
+++ tests/parsers/rest/test_group.py
@@ -119,7 +119,47 @@ def test_nothing_after_group(tmp_path: Path) -> None:
     assert document.namespace["x"] == [1, 2]
 
 
+def test_empty_group(tmp_path: Path) -> None:
+    """
+    The group parser groups examples even when the group is empty.
+    """
+    content = """\
+
+    .. code-block:: python
+
+        x = []
+
+    .. group: start
+
+    .. group: end
+
+    .. code-block:: python
+
+        x = [*x, 3]
+    """
+
+    test_document = tmp_path / "test.rst"
+    test_document.write_text(data=content, encoding="utf-8")
+
+    group_parser = GroupParser(directive="group")
+    code_parser = PythonCodeBlockParser()
+
+    sybil = Sybil(parsers=[code_parser, group_parser])
+    document = sybil.parse(path=test_document)
+
+    for example in document.examples():
+        example.evaluate()
+
+    parsed_examples = [example.parsed for example in document.examples()]
+    expected = [
+        "x = []\n",
+        "x = [*x, 3]",
+    ]
+    assert parsed_examples == expected
+
+    assert document.namespace["x"] == [3]
+
+
 # TODO: With skips before / in the middle
-# TODO: With an empty group
 # TODO: With end before start / without start
 # TODO: With different evaluators in the middle
