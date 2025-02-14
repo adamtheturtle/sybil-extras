6ccf2c8a6867178c2eabdd497313410d367c944e
Adam Dangoor <adamdangoor@gmail.com>
2025-02-14 09:39:28 +0000

    Add MyST parser

--- src/sybil_extras/parsers/myst/grouped_code_block.py
+++ src/sybil_extras/parsers/myst/grouped_code_block.py
@@ -1,3 +1,29 @@
 """
 A group parser for MyST.
 """
+
+from sybil.parsers.markdown.lexers import DirectiveInHTMLCommentLexer
+from sybil.parsers.myst.lexers import (
+    DirectiveInPercentCommentLexer,
+)
+
+from sybil_extras.parsers.abstract.grouped_code_block import (
+    AbstractGroupedCodeBlockParser,
+)
+
+
+class GroupedCodeBlockParser(AbstractGroupedCodeBlockParser):
+    """
+    A code block group parser for MyST.
+    """
+
+    def __init__(self, directive: str) -> None:
+        """
+        Args:
+            directive: The name of the directive to use for grouping.
+        """
+        lexers = [
+            DirectiveInPercentCommentLexer(directive=directive),
+            DirectiveInHTMLCommentLexer(directive=directive),
+        ]
+        super().__init__(lexers=lexers)
