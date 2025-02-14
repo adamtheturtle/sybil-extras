2470f6a1c31d01d17bedd0f52dde1f2386672dcd
Adam Dangoor <adamdangoor@gmail.com>
2025-02-14 09:32:10 +0000

    Fix docformatter

--- src/sybil_extras/parsers/rest/grouped_code_block.py
+++ src/sybil_extras/parsers/rest/grouped_code_block.py
@@ -2,22 +2,14 @@
 A group parser for reST.
 """
 
-from collections.abc import Iterable, Sequence
-
-from sybil import Document, Region
-from sybil.parsers.abstract.lexers import LexerCollection
 from sybil.parsers.rest.lexers import DirectiveInCommentLexer
-from sybil.region import Lexeme
+
+from sybil_extras.parsers.abstract.grouped_code_block import (
+    AbstractGroupedCodeBlockParser,
+)
 
 
-def _validate_sub_regions(regions: Sequence[Region]) -> None:
-    evaluators = {region.evaluator for region in regions}
-    if len(evaluators) != 1:
-        message = "All sub-regions of a group must have the same evaluator."
-        raise ValueError(message)
-
-
-class GroupedCodeBlockParser:
+class GroupedCodeBlockParser(AbstractGroupedCodeBlockParser):
     """
     A code block group parser for reST.
     """
@@ -27,50 +19,5 @@ class GroupedCodeBlockParser:
         Args:
             directive: The name of the directive to use for grouping.
         """
-        self.lexers: LexerCollection = LexerCollection(
-            [DirectiveInCommentLexer(directive=directive)]
-        )
-
-    def __call__(self, document: Document) -> Iterable[Region]:
-        """
-        Yield regions to evaluate, grouped by start and end comments.
-        """
-        lexed_regions = self.lexers(document)
-        start_end_pairs = [
-            (first, second)
-            for first, second in zip(lexed_regions, lexed_regions, strict=True)
-        ]
-        current_sub_regions: Sequence[Region] = []
-        for start_region, end_region in start_end_pairs:
-            current_sub_regions = []
-            for _, region in document.regions:
-                if start_region.start < region.start < end_region.end:
-                    current_sub_regions = [*current_sub_regions, region]
-                elif region.start > end_region.start:
-                    break
-            if current_sub_regions:
-                _validate_sub_regions(regions=current_sub_regions)
-                first_sub_region = current_sub_regions[0]
-
-                parsed_text = "\n".join(
-                    region.parsed for region in current_sub_regions
-                )
-
-                parsed = Lexeme(
-                    text=parsed_text,
-                    line_offset=first_sub_region.parsed.line_offset,
-                    offset=first_sub_region.parsed.offset,
-                )
-
-                for current_sub_region in current_sub_regions:
-                    document.regions.remove(
-                        (current_sub_region.start, current_sub_region)
-                    )
-
-                yield Region(
-                    start=first_sub_region.start,
-                    end=first_sub_region.end,
-                    parsed=parsed,
-                    evaluator=first_sub_region.evaluator,
-                )
-                break
+        lexers = [DirectiveInCommentLexer(directive=directive)]
+        super().__init__(lexers=lexers)
