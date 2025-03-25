550ce417d6cdaf768ca849a645dadb7f43d51ffe
Adam Dangoor <adamdangoor@gmail.com>
2025-03-25 13:38:55 +0000

    Add stubs for Jinja2 tests

--- tests/parsers/rest/test_sphinx_jinja2.py
+++ tests/parsers/rest/test_sphinx_jinja2.py
@@ -1,3 +1,41 @@
 """
 Tests for the sphinx-jinja2 parser for reST.
 """
+
+from pathlib import Path
+
+from sybil import Sybil
+from sybil.example import Example
+
+
+def test_sphinx_jinja2(*, tmp_path: Path) -> None:
+    """
+    The sphinx-jinja2 parser extracts information from jinja blocks.
+    """
+    # These examples are taken from the sphinx-jinja2 documentation.
+    content = """\
+    .. jinja::
+       :ctx: {"name": "World"}
+
+       Hallo {{ name }}!
+
+    .. jinja::
+       :file: templates/example1.jinja
+       :ctx: {"name": "World"}
+    """
+
+    test_document = tmp_path / "test.rst"
+    test_document.write_text(data=content, encoding="utf-8")
+
+    def evaluator(example: Example) -> None:
+        pass
+
+    parser = SphinxJinja2Parser(evaluator=evaluator)
+    sybil = Sybil(parsers=[parser])
+    document
+
+
+def test_write_with_shell_evaluator(*, tmp_path: Path) -> None:
+    """
+    It is possible to write to empty and non-empty jinja blocks.
+    """
