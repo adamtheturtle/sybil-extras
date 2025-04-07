0f8a12b1a1cae8212a3eb35465573a4c34bb1f2d
Adam Dangoor <adamdangoor@gmail.com>
2025-04-07 22:15:23 +0100

    Simplify some logic by combining some if statements

--- src/sybil_extras/evaluators/shell_evaluator.py
+++ src/sybil_extras/evaluators/shell_evaluator.py
@@ -421,20 +421,21 @@ class ShellCommandEvaluator:
             new_code_block_content=new_region_content,
         )
 
-        existing_file_content = example.document.text
-
-        if modified_region_text != original_region_text:
+        if (
+            modified_region_text != original_region_text
+            and self._write_to_file
+        ):
+            existing_file_content = example.document.text
             modified_document_content = (
                 existing_file_content[: example.region.start]
                 + modified_region_text
                 + existing_file_content[example.region.end :]
             )
-            if self._write_to_file:
-                example.document.text = modified_document_content
-                Path(example.path).write_text(
-                    data=modified_document_content,
-                    encoding=self._encoding,
-                )
+            example.document.text = modified_document_content
+            Path(example.path).write_text(
+                data=modified_document_content,
+                encoding=self._encoding,
+            )
 
         if result.returncode != 0:
             raise subprocess.CalledProcessError(
