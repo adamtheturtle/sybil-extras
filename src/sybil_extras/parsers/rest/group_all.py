54b778cb0a6c29a1cf6bc93016bee0430ad841a1
Adam Dangoor <adamdangoor@gmail.com>
2025-11-19 07:10:41 +0000

    Remove useless __init__ methods from GroupAllParser subclasses
    The __init__ methods in the subclasses were just delegating to super()
    with the same arguments, which pylint correctly identified as useless.
    Since the subclasses don't add any additional initialization logic,
    they can inherit __init__ directly from AbstractGroupAllParser.
    
    This makes each subclass even simpler (~11 lines each) and achieves
    a perfect 10.00/10 pylint score.

--- src/sybil_extras/parsers/rest/group_all.py
+++ src/sybil_extras/parsers/rest/group_all.py
@@ -2,8 +2,6 @@
 A parser that groups all code blocks in a reStructuredText document.
 """
 
-from sybil.typing import Evaluator
-
 from sybil_extras.parsers.abstract.group_all import AbstractGroupAllParser
 
 
@@ -11,19 +9,3 @@ class GroupAllParser(AbstractGroupAllParser):
     """
     A parser that groups all code blocks in a document without markup.
     """
-
-    def __init__(
-        self,
-        *,
-        evaluator: Evaluator,
-        pad_groups: bool,
-    ) -> None:
-        """
-        Args:
-            evaluator: The evaluator to use for evaluating the combined region.
-            pad_groups: Whether to pad groups with empty lines.
-                This is useful for error messages that reference line numbers.
-                However, this is detrimental to commands that expect the file
-                to not have a bunch of newlines in it, such as formatters.
-        """
-        super().__init__(evaluator=evaluator, pad_groups=pad_groups)
