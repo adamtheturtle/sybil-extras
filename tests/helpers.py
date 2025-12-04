3f485f4a1d04bd1fd7c4d7bd1ae3993944729686
Adam Dangoor <adamdangoor@gmail.com>
2025-12-04 16:12:29 +0000

    Simplify helper

--- tests/helpers.py
+++ tests/helpers.py
@@ -10,5 +10,4 @@ def join_markup(language: MarkupLanguage, *parts: str) -> str:
     Join markup fragments using the language's separator, trimming edge
     newlines.
     """
-    cleaned_parts = [part.strip("\n") for part in parts if part]
-    return language.markup_separator.join(cleaned_parts)
+    return language.markup_separator.join(parts)
