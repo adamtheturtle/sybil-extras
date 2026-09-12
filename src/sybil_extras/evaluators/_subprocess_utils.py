8be2a3d331f87d5fc88ae874a579520718b58882
Adam Dangoor <adamdangoor@gmail.com>
2026-09-12 12:59:52 +0100

    Use the platform guard for openpty

--- src/sybil_extras/evaluators/_subprocess_utils.py
+++ src/sybil_extras/evaluators/_subprocess_utils.py
@@ -56,13 +56,9 @@ def run_command(
     if use_pty:
         stdout_master_fd: int = -1
         slave_fd: int = -1
-        # We use ``hasattr`` rather than
-        # ``contextlib.suppress(AttributeError)`` so that ``mypy`` can narrow
-        # the type on Windows, where ``os.openpty`` does not exist.
-        # We also check ``sys.platform`` so that pyright can narrow the type.
-        if sys.platform != "win32" and hasattr(  # pylint: disable=bad-builtin
-            os, "openpty"
-        ):  # pragma: no branch
+        # ``os.openpty`` is unavailable on Windows. The platform check also
+        # lets static type checkers narrow to platforms where it exists.
+        if sys.platform != "win32":  # pragma: no branch
             stdout_master_fd, slave_fd = os.openpty()
 
         stdout: int = slave_fd
